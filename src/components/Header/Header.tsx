b4d9534d1d5de1204ea3a05a2db68c54b2f67248
Arthur Lopes <arthurlopr12@gmail.com>
2023-10-20 21:07:43 -0300

    feat: create header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,31 @@
-import { Container } from "./styles";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../../../public/images/white-pokeball.svg";
+import {
+  Container,
+  Logo,
+  ScheduleAppointmentContainer,
+  Wrapper,
+} from "./styles";
 
 export function Header() {
-  return <Container>teste</Container>;
+  return (
+    <>
+      <Container>
+        <Logo>
+          <Link href="/">
+            <Image alt="logo" src={logo} width={34} height={34} />
+            <p>Centro Pokémon</p>
+          </Link>
+        </Logo>
+
+        <Wrapper>
+          <Link href="/about">Quem Somos</Link>
+          <ScheduleAppointmentContainer>
+            <Link href="/scheduling">Agendar Consulta</Link>
+          </ScheduleAppointmentContainer>
+        </Wrapper>
+      </Container>
+    </>
+  );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,7 +1,59 @@
+import { lighten } from "polished";
 import styled from "styled-components";
 import { theme } from "../../styles/theme/theme";
 
 export const Container = styled.div`
   width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  background-color: ${theme.colors.white};
+
+  padding: 22px 83px;
+`;
+
+export const Wrapper = styled.div`
+  align-items: end;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  gap: 30px;
+`;
+
+export const Logo = styled.div`
+  width: 62px;
+  border-radius: 50%;
+  padding: 14px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   background-color: ${theme.colors.red};
+
+  position: relative;
+
+  a {
+    width: 34px;
+    height: 34px;
+  }
+`;
+
+export const ScheduleAppointmentContainer = styled.div`
+  background-color: ${theme.colors.red};
+  color: ${theme.colors.white};
+  transition: background-color 0.2s ease-in-out;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 13px 24px;
+  border-radius: 30px;
+
+  &:hover {
+    background-color: ${lighten(0.1, theme.colors.red)};
+  }
 `;
